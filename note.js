ad3e0176f25ccaff711da4bf9dae9fa59faa2f70
Benedictus Mwanthi <benedictusyohana5@gmail.com>
2023-09-14 20:56:28 +0300

    save text to local storage

---
+++ note.js
@@ -0,0 +1,6 @@
+//Get references to the textarea and buttons
+const editor = document.getElementById("editor");
+const saveButton = document.getElementById("save");
+const clearButton = document.getElementById("clear")
+
+//Function to save the text to local storage
